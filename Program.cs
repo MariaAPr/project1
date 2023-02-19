934f62cadb439c1ab94ac7300398b7f8842664fc
Prokopenkova <ProkopenkovaM@terralink-global.com>
2023-02-19 21:31:41 +0300

    scheme was added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] firstMassive = new string[4] {"dog", "cat", "bird", "mouse"};
+
+int CountElementsOfFinalMassive(string[] firstMassive)
+    {
+        int counter = 0;
+        for (int index = 0; index < firstMassive.Length; index++)
+        {
+            if (firstMassive[index].Length <= 3) {
+            counter++;
+            }
+        }
+        
+        return counter;
+    }
+
+int a = CountElementsOfFinalMassive(firstMassive);
+
+string[] finalMassive = new string[a];
+
+for (int index = 0; index < firstMassive.Length; index++)
+    {
+        if (firstMassive[index].Length <= 3) {
+        finalMassive[index] = firstMassive[index];
+        }
+    }
+
+Console.WriteLine(String.Join(",", finalMassive));
